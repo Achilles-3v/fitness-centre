c570ce0842e511ef3f2336f91c7205c9cc5bce26
Achilles-3v <sh17vv@gmail.com>
2023-11-30 02:22:31 +0500

    printClubOptions and getChoice methods added

--- src/members/MembershipManagement.java
+++ src/members/MembershipManagement.java
@@ -17,11 +17,29 @@ public class MembershipManagement {
             }
             catch (InputMismatchException e) {
                 reader.nextLine();
-                System.out.println("ERROR: INVALID INPUT. Please try again: ");
+                System.out.println("/nERROR: INVALID INPUT. Please try again: ");
             }
         }
         return choice;
     }
 
+    private void printClubOptions() {
+        System.out.println("1) Club Mercury");
+        System.out.println("2) Club Neptune");
+        System.out.println("3) Club Jupiter");
+        System.out.println("4) Multi Clubs");
+    }
 
+    public int getChoice() {
+        int choice;
+
+        System.out.println("WELCOME TO OZONE FITNESS CENTER");
+        System.out.println("===============================");
+        System.out.println("1) Add Member");
+        System.out.println("2) Remove Member");
+        System.out.println("3) Display Member Information");
+        System.out.print("Please select an option (or Enter -1 to quit): ");
+
+        return choice = getIntInput();
+    }
 }
