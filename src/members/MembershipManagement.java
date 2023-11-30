782625631246617f810476e29cd7708df19ec9a0
Achilles-3v <sh17vv@gmail.com>
2023-11-30 18:45:29 +0500

    removeMember and printMemberInfo methods added

--- src/members/MembershipManagement.java
+++ src/members/MembershipManagement.java
@@ -106,5 +106,40 @@ public class MembershipManagement {
         return mem;
     }
 
+    public void removeMember(LinkedList<Member> m) {
+        int memberID;
+        System.out.print("Please, enter member ID to remove: ");
+        memberID = getIntInput();
+        for (int i = 0; i < m.size(); i++) {
+            if (m.get(i).getMemberID() == memberID) {
+                m.remove(i);
+                System.out.println("/nThis member removed/n");
+                return;
+            }
+        }
 
+        System.out.println("/nMember with ID " + memberID + " not found!/n");
+    }
+
+    public void printMemberInfo(LinkedList<Member> m) {
+        int memberID;
+        System.out.println("Please, enter member ID to display information: ");
+        memberID = getIntInput();
+        for (int i = 0; i < m.size(); i++) {
+            if (m.get(i).getMemberID() == memberID) {
+                String[] memberInfo = m.get(i).toString().split(", ");
+                System.out.println("/n/nMember Type: " + memberInfo[0]);
+                System.out.println("Member ID: " + memberInfo[1]);
+                System.out.println("Member Name: " + memberInfo[2]);
+                System.out.println("Member Fees: " + memberInfo[3]);
+                if (memberInfo[0].equals("S")) {
+                    System.out.println("Club ID: " + memberInfo[4]);
+                }
+                else
+                    System.out.println("Membership Points: " + memberInfo[4]);
+                return;
+            }
+        }
+        System.out.println("/nMember ID not found!/n");
+    }
 }
